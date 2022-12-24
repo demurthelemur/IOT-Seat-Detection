37c2ce1a8b0cb9fb4950d1bb667dfefdb6c3a58a
Bilgesu Çakır <95298678+bilgesucakir@users.noreply.github.com>
2022-12-24 21:04:06 +0300

    image draw for 4 seats added
    enter upper left, width, length for 4 seats to define the rois

--- img-rectangle-draw.py
+++ img-rectangle-draw.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image
 
-im = Image.open('C:/Users/USER/Pictures/Camera Roll/WIN_20221224_19_32_49_Pro.jpg')
+im = Image.open('WIN_20221224_19_32_49_Pro.jpg')
 
 # Create figure and axes
 fig, ax = plt.subplots()
@@ -10,26 +10,27 @@ fig, ax = plt.subplots()
 # Display the image
 ax.imshow(im)
 
-input1 = str(input(print("enter first point in x,y format (upper left):")))
-input2 = str(input(print("enter width and length:")))
+print("for defining 4 seats:")
 
 
-inputs1 = input1.split(",")
-inputs2 = input2.split(",")
+colors = ['r', 'b', 'g', 'y']
+for i in range(1,5):
+    print()
+    print("seat"+str(i))
+    input1 = str(input("enter first point in x,y format (upper left):"))
+    input2 = str(input("enter width and length:"))
+
+    inputs1 = input1.split(",")
+    inputs2 = input2.split(",")
+
+    first_x, first_y = int(inputs1[0]), int(inputs1[1])
+
+    # Create a Rectangle patch
+    rect = patches.Rectangle((first_x, first_y), int(inputs2[0]), int(inputs2[1]), linewidth=1, edgecolor=colors[i-1], facecolor='none')
+
+    # Add the patch to the Axes
+    ax.add_patch(rect)
 
 
-first_x, first_y = int(inputs1[0]), int(inputs1[1])
-
-
-
-
-
-# Create a Rectangle patch
-rect = patches.Rectangle((first_x, first_y), int(inputs2[0]), int(inputs2[1]), linewidth=1, edgecolor='r', facecolor='none')
-
-
-
-# Add the patch to the Axes
-ax.add_patch(rect)
-
+print("image with 4 seats drawn:")
 plt.show()
